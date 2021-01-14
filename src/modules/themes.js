928fb1c47e8441087ce2721d4fb234dade432bd6
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2021-01-14 01:39:56 -0500

    First finished version of todo-it settings

--- src/modules/themes.js
+++ src/modules/themes.js
@@ -1,15 +1,97 @@
 'use strict'
+import { manage } from '/src/helpers.js'
 
+//DOM settings [data/themes] stuff
 const themes = [
-    {   //light blue-green default
-        sort: `rgba(20, 189, 96,0.4)`, inner: `rgba(20, 189, 96,0.4)`
-    },
     {   //red 
-        sort: `rgba(161,16,15,0.95)`, inner: `rgb(175,36,36)`, header:`rgb(139,20,20)`
-    }, 
-    { 
-        sort: 'rgba(16,15,161,0.95)', inner: 'rgb(36,36,175)', header: `rgb(20,20,139)` 
+        sort: `rgba(161,16,15,0.95)`, inner: `rgb(175,36,36)`, header: `rgb(139,20,20)`
+    },
+    {
+        sort: 'rgba(16,15,161,0.95)', inner: 'rgb(36,36,175)', header: `rgb(20,20,119)`
+    },
+    {
+        sort: 'rgba(15,161,16,0.95)', inner: 'rgb(36,175,76)', header: `rgb(20,139,65)`
+    },
+    {
+        sort: 'rgba(255,255,75,0.7)', inner: 'rgb(255,255,102)', header: `rgb(255,255,12)`
     }
 ]
 
-    export {themes}
+const COG = (() => {
+    const settings =
+        manage.elWithClasses('', 'settings-container', '', 'div');
+    const settingsTab =
+        manage.elWithClasses('', 'settings-tab', '', 'div');
+    const tabs =
+        manage.elWithClasses('', 'settings-tabs', '', 'div');
+    const settingsMain =
+        manage.elWithClasses('', 'settings-content', '', 'div');
+
+    //themes content
+    const themesCont =
+        manage.elWithClasses('', 'themes-container', '', 'form');
+    const themesSelectionPara =
+        manage.createPara('Select theme:', 'lbl-theme');
+    const theme_red =
+        manage.createRadio('thred', 'theme-red', 'themes');
+    const lbl_red =
+        manage.createLabel('red', '', 'lbl-red', 'lbl-colors');
+    const theme_blue =
+        manage.createRadio('thblue', 'theme-blue', 'themes');
+    const lbl_blue =
+        manage.createLabel('blue', '', 'lbl-blue', 'lbl-colors');
+    const theme_green =
+        manage.createRadio('thgreen', 'theme-green', 'themes');
+    const lbl_green =
+        manage.createLabel('green', '', 'lbl-green', 'lbl-colors');
+    const theme_yellow =
+        manage.createRadio('thyellow', 'theme-yellow', 'themes');
+    const lbl_yellow =
+        manage.createLabel('yellow', '', 'lbl-yellow', 'lbl-colors');
+    const btnThemes =
+        manage.elWithClasses('', '', 'btn-tabs', 'div');
+    const btnClearData =
+        manage.elWithClasses('Clear Local Data', 'clear-data', '', 'button');
+    const btnData =
+        manage.elWithClasses('', '', 'btn-tabs', 'div');
+    const settingsBtnCont =
+        manage.elWithClasses('', 'settings-btns', '', 'div');
+
+    const btnApply =
+        manage.createSubmit('Apply');
+    const btnClose =
+        manage.elWithClasses('Close', 'settings-close', '', 'button');
+    return {
+        settings, settingsTab, settingsMain,
+        settingsBtnCont, themesCont, btnClearData,
+        btnThemes, btnData, tabs,
+        themesSelectionPara,
+        theme_red, lbl_red,
+        theme_blue, lbl_blue,
+        theme_green, lbl_green,
+        theme_yellow, lbl_yellow,
+        btnApply, btnClose
+
+    }
+})();
+
+//DOM properties, contents and the appended family tree of the settings section
+const visualSettings = () => {
+    document.getElementById('content').appendChild(COG.settings);
+    COG.settings.append(COG.settingsTab);
+    COG.settingsTab.append(COG.tabs, COG.settingsMain);
+    COG.tabs.append(COG.btnThemes, COG.btnData);
+    COG.btnThemes.appendChild(manage.createPara('Themes', ''));
+    COG.btnData.appendChild(manage.createPara('Data', ''));
+    COG.settings.style.display = 'none';
+    COG.lbl_red.appendChild(COG.theme_red);
+    COG.lbl_blue.appendChild(COG.theme_blue);
+    COG.lbl_green.appendChild(COG.theme_green);
+    COG.lbl_yellow.appendChild(COG.theme_yellow);
+    COG.themesCont.append(
+        COG.themesSelectionPara,
+        COG.lbl_red, COG.lbl_blue,
+        COG.lbl_green, COG.lbl_yellow);
+}
+
+export { themes, COG, visualSettings }
