d743a566a50a52f6c4099ffca7d8ca467e5ed65b
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2021-01-14 01:36:45 -0500

    Modified few function parameters

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//This file consist of custom function that mainly create or manage DOM-related stuff a.k.a helpers
+//This file contains custom functions that mainly are created to manage DOM related stuff a.k.a helpers
 class manage {
     static createPara(txt, eId) {
         const el = document.createElement('p');
@@ -19,27 +19,31 @@ class manage {
         eClassName !== '' ? input.className = eClassName : undefined;
         return input;
     }
-    static createTextarea(eClassName, cols, rows, disabled, value){
+    static createTextarea(eClassName, cols, rows, disabled, placeholder){
         const textarea = document.createElement('textarea');
         textarea.className = eClassName;
         textarea.cols = cols;
         textarea.rows = rows;
         textarea.disabled = disabled;
-        textarea.value = value;
+        textarea.placeholder = placeholder;
         eClassName !== '' ? textarea.className = eClassName : undefined;
         return textarea;
     }
-    static createDate(value, min){
+    static createDate(eId, value, min){
         const date = document.createElement('input');
         date.type = "date";
         date.value = value;
         date.min = min;
+        date.id = eId;
+        date.required = true;
         return date;
     }
-    static createLabel(forVal, text){
+    static createLabel(forVal, text, eId, eClassName){
         const label = document.createElement('label');
         label.setAttribute("for",forVal);
         label.textContent = text;
+        label.id = eId;
+        label.className = eClassName;
         return label;
     }
     static createSelect(eId){
@@ -59,11 +63,18 @@ class manage {
         input.value = text;
         return input;
     }
-    static createChecklist(eClassName, checked){
+    static createRadio(value,eId,name){
+        const input = document.createElement('input');
+        input.type = "radio";
+        input.value = value;
+        input.id = eId;
+        input.name = name;
+        return input;
+    }
+    static createChecklist(eClassName){
         const input = document.createElement('input');
         input.type = 'checkbox';
-        input.checked = checked;
-        input.ClassName = eClassName;
+        input.className = eClassName;
         return input;
     }
 
